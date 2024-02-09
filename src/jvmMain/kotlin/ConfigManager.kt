f0f888b1f72c2f00f5315733dda9f8b98f3beb83
hao88 <52927295@qq.com>
2024-02-09 16:51:28 +0800

    设定IP方式

--- src/jvmMain/kotlin/ConfigManager.kt
+++ src/jvmMain/kotlin/ConfigManager.kt
@@ -6,33 +6,10 @@ import java.io.File
 
 object ConfigManager {
     private const val CONFIG_FILE_PATH = "config.json" // or "config.yml" for YAML
-    private const val CONFIG_FILE_USER_PATH = "config.json"
     fun loadConfig(): Config {
-        val customConfig = File(CONFIG_FILE_USER_PATH)
-        if (customConfig.exists()) {
-            val textJson = customConfig.bufferedReader().use {
-                it.readText()
-            }
-            return Json.decodeFromString(textJson)
-        }
         useResource(CONFIG_FILE_PATH) { stream ->
             val textJson = stream.bufferedReader().use { it.readText() }
             return Json.decodeFromString(textJson)
-        }
-    }
-
-    fun saveConfig(config: Config) {
-        val currentDirectory = System.getProperty("user.dir")
-        val file = File(CONFIG_FILE_USER_PATH)
-        if (!file.exists()) {
-            file.createNewFile()
-            useResource(CONFIG_FILE_PATH) {
-                it.copyTo(file.outputStream())
-            }
-        } else {
-            file.bufferedWriter().use {
-                it.write(Json.encodeToString(config))
-            }
         }
     }
 }
